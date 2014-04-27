79b8717116da6bf113f35007b43b93474f7a3e4d
mfcr <manuel_campelo@yahoo.com>
2014-04-27 21:44:39 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,11 +13,10 @@ Y <- rbind(read.table("UCI HAR Dataset/train/y_train.txt"), read.table("UCI HAR 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
 feat <- read.table("UCI HAR Dataset/features.txt")
-mean_std_feat <- grep("-mean\\(\\)|-std\\(\\)", feat[, 2])
+mean_std_feat <- grep("-mean\\(\\)|-std\\(\\)", feat[, 2]) #colmn # of the featrures with mean or std in their names.
 X <- X[, mean_std_feat]
 names(X) <- feat[mean_std_feat, 2]
-names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X)) 
+names(X) <- tolower(gsub("\\(|\\)", "", names(X)))
 
 #------------------------------------------------------------------------------------
 # 3. Uses descriptive activity names to name the activities in the data set
@@ -33,19 +32,19 @@ cleaned <- cbind(S, Y, X) #merge subjects, mean and std data and activities tabl
 # write.table(cleaned, "cleaned_data.txt")---> Optional saving of the data before averaging.
 
 #------------------------------------------------------------------------------------
-# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 Subjects = unique(S)[,1]
-num_Subjects = length(unique(S)[,1])
+num_Subjects = length(Subjects)
 num_Activities = length(activities[,1])
 num_Cols = dim(cleaned)[2]
 result = cleaned[1:(num_Subjects*num_Activities), ]
 
 row_count = 1
-for (s in 1:num_Subjects) {
+for (su in 1:num_Subjects) {
   for (nu in 1:num_Activities) {
-    result[row, 1] = Subjects[s]
+    result[row, 1] = Subjects[su]
     result[row, 2] = activities[a, 2]
-    tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[nu, 2], ]
+    tmp <- cleaned[cleaned$subject==su & cleaned$activity==activities[nu, 2], ]
     result[row_count, 3:num_Cols] <- colMeans(tmp[, 3:num_Cols])
     row_count = row_count+1
   }
